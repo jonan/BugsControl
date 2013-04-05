e260df3d20de8a87d9d0182e7c57de0c42c297f8
Jon Ander Peñalba <jonan88@gmail.com>
2013-04-05 20:54:10 +0200

    Use ActionBarSherlock

--- src/jp/bugscontrol/ActivityHome.java
+++ src/jp/bugscontrol/ActivityHome.java
@@ -1,22 +1,13 @@
 package jp.bugscontrol;
 
+import com.actionbarsherlock.app.SherlockActivity;
+
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class ActivityHome extends Activity {
-
+public class ActivityHome extends SherlockActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_home, menu);
-        return true;
-    }
-
 }

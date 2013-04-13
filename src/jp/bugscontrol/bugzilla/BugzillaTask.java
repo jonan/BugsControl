5a7095be870087c27b06d2c3cf836a00c19a26ac
Jon Ander Peñalba <jonan88@gmail.com>
2013-04-13 15:43:04 +0200

    Do Bugzilla calls through POST

--- src/jp/bugscontrol/bugzilla/BugzillaTask.java
+++ src/jp/bugscontrol/bugzilla/BugzillaTask.java
@@ -1,42 +1,56 @@
 package jp.bugscontrol.bugzilla;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
+import java.util.UUID;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import jp.bugscontrol.bugzilla.Server.Listener;
 
 import android.os.AsyncTask;
 
 public class BugzillaTask extends AsyncTask<Void, Void, Void> {
-    String method, response;
+    String method, params, response;
     Listener listener;
 
     public BugzillaTask(String method, Listener listener) {
         this.method = method;
+        this.params = "";
         this.listener = listener;
     }
 
     public BugzillaTask(String method, String params, Listener listener) {
-        this.method = method + "&params=[{" + params + "}]";
+        this.method = method;
+        this.params = params;
         this.listener = listener;
     }
 
     @Override
-    protected Void doInBackground(Void... params) {
+    protected Void doInBackground(Void... p) {
         try {
-            URL challenges = new URL("https://bugs.kde.org/jsonrpc.cgi?method=" + method);
-            URLConnection connection = challenges.openConnection();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            StringBuilder builder = new StringBuilder();
-            String line = reader.readLine();
-            while (line != null) {
-                builder.append(line);
-                line = reader.readLine();
-            }
-            reader.close();
-            response = builder.toString();
+            HttpClient http_client = new DefaultHttpClient();
+            HttpPost http_post = new HttpPost("https://bugs.kde.org/jsonrpc.cgi");
+            http_post.addHeader("Content-Type", "application/json");
+
+            JSONObject request = new JSONObject();
+            request.put("id", UUID.randomUUID().hashCode());
+            request.put("method", method);
+            JSONArray array;
+            if (params.length() > 0)
+                array = new JSONArray("[{" + params + "}]");
+            else
+                array = new JSONArray();
+            request.put("params", array);
+
+            http_post.setEntity(new StringEntity(request.toString()));
+            HttpEntity entity = http_client.execute(http_post).getEntity();
+            response = EntityUtils.toString(entity);
         } catch (Exception e) {
             e.printStackTrace();
         }

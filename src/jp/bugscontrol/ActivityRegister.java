6267238b0811432da1edbd64637b5eb9ca0a6ced
Jon Ander Peñalba <jonan88@gmail.com>
2013-04-18 00:09:43 +0200

    Added base files for a new activity where registrations will be done

---
+++ src/jp/bugscontrol/ActivityRegister.java
@@ -0,0 +1,13 @@
+package jp.bugscontrol;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+import android.os.Bundle;
+
+public class ActivityRegister extends SherlockActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}

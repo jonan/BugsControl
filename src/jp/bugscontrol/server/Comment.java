8b1aad3dc8e96b9bec6e9129d0d8a82654857d22
Jon Ander Peñalba <jonan88@gmail.com>
2013-07-24 17:19:12 +0200

    Create a class to store comments

---
+++ src/jp/bugscontrol/server/Comment.java
@@ -0,0 +1,40 @@
+/*
+ *  BugsControl
+ *  Copyright (C) 2013  Jon Ander Peñalba
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package jp.bugscontrol.server;
+
+public abstract class Comment {
+    protected int id;
+    protected String text, creator;
+    protected Bug bug;
+
+    protected Product product;
+
+    public Comment(Bug bug) {
+        this.bug = bug;
+    }
+
+    abstract public void createFromString(String s);
+
+    public int getId() {return id;}
+
+    public String getText()    {return text;}
+    public String getCreator() {return creator;}
+
+    public Bug getBug() {return bug;}
+}

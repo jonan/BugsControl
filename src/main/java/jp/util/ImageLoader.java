77fe82d9ce90adcc68f625083ffe3d64594e257f
Jon Ander Peñalba <jonan88@gmail.com>
2014-10-11 21:35:10 +0200

    Remove workaround for an old Android bug

--- src/main/java/jp/util/ImageLoader.java
+++ src/main/java/jp/util/ImageLoader.java
@@ -18,78 +18,50 @@
 
 package jp.util;
 
-import java.io.FilterInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.widget.ImageView;
 
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ImageLoader extends AsyncTask<Void, Void, Bitmap> {
-    String src;
-    ImageView view;
+    public static Map<String, Bitmap> cache = new HashMap<String, Bitmap>();
 
-    public static Map<String,Bitmap> cache = new HashMap<String,Bitmap>();
+    private final String src;
+    private final ImageView view;
 
-    public static void loadImage(String src, ImageView view) {
-        if (cache.containsKey(src))
+    public static void loadImage(final String src, final ImageView view) {
+        if (cache.containsKey(src)) {
             view.setImageBitmap(cache.get(src));
-        else
+        } else {
             (new ImageLoader(src, view)).execute();
+        }
     }
 
-    private ImageLoader(String src, ImageView view) {
+    private ImageLoader(final String src, final ImageView view) {
         this.src = src;
         this.view = view;
     }
 
     @Override
     protected Bitmap doInBackground(Void... params) {
-        Bitmap image = null;
         try {
-            URL url = new URL(src);
-            image = BitmapFactory.decodeStream(new FlushedInputStream(url.openStream()));
+            final URL url = new URL(src);
+            return BitmapFactory.decodeStream(url.openStream());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return image;
+        return null;
     }
 
     @Override
-    protected void onPostExecute(Bitmap result) {
+    protected void onPostExecute(final Bitmap result) {
         if (result != null) {
             cache.put(src, result);
             view.setImageBitmap(result);
-        }
-    }
-
-    // This class addresses an Android bug
-    static class FlushedInputStream extends FilterInputStream {
-        public FlushedInputStream(InputStream inputStream) {
-            super(inputStream);
-        }
-
-        @Override
-        public long skip(long n) throws IOException {
-            long totalBytesSkipped = 0L;
-            while (totalBytesSkipped < n) {
-                long bytesSkipped = in.skip(n - totalBytesSkipped);
-                if (bytesSkipped == 0L) {
-                    int b = read();
-                    if (b < 0) {
-                        break;  // EOF
-                    } else {
-                        bytesSkipped = 1; // Read one byte
-                    }
-                }
-                totalBytesSkipped += bytesSkipped;
-            }
-            return totalBytesSkipped;
         }
     }
 }
